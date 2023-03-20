b37288a8007e833192a473605254d739c193707e
mustafath <mustafadalkesen11@gmail.com>
2023-03-20 19:46:55 +0300

    custom view modifier

---
+++ SwiftulLearningAdvancedLearnin/ButtonStyleBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonStyleBootcamp.swift
+//  SwiftulLearningAdvancedLearnin
+//
+//  Created by Mustafa Girgin on 20.03.2023.
+//
+
+import SwiftUI
+
+struct ButtonStyleBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonStyleBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStyleBootcamp()
+    }
+}
