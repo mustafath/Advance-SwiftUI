5d5bd74e8fe32ec0d35dc54d43ddf7b17e76ee3a
mustafath <mustafadalkesen11@gmail.com>
2023-03-20 18:58:33 +0300

    custom view modifier

---
+++ SwiftulLearningAdvancedLearnin/ViewModifierBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModifierBootcamp.swift
+//  SwiftulLearningAdvancedLearnin
+//
+//  Created by Mustafa Girgin on 20.03.2023.
+//
+
+import SwiftUI
+
+struct ViewModifierBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewModifierBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewModifierBootcamp()
+    }
+}
