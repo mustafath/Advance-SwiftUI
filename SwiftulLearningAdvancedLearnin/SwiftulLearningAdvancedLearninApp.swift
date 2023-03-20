01703b00ee0920dda7595e6ab6500f4ea77bd538
mustafath <mustafadalkesen11@gmail.com>
2023-03-20 18:41:13 +0300

    Initial Commit

---
+++ SwiftulLearningAdvancedLearnin/SwiftulLearningAdvancedLearninApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulLearningAdvancedLearninApp.swift
+//  SwiftulLearningAdvancedLearnin
+//
+//  Created by Mustafa Girgin on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulLearningAdvancedLearninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
